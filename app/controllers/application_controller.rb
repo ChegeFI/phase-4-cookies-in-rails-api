d2ffedde0c4fc77708ce405adf1216ac42e14628
Chege Ian <chegeian65@gmail.com>
2022-12-02 18:39:00 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
-class ApplicationController < ActionController::API
-  include ActionController::Cookies
+class ArticlesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def index
+    articles = Article.all.includes(:user).order(created_at: :desc)
+    render json: articles, each_serializer: ArticleListSerializer
+  end
+
+  def show
+    article = Article.find(params[:id])
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] <= 3
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: 401
+    end
+  end
+
+  private
+
+  def record_not_found
+    render json: { error: "Article not found" }, status: :not_found
+  end
+
 end
